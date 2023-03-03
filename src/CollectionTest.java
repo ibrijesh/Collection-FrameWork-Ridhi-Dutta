02699fcda9c1d50671aac887059b83e79da962a9
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-03-03 23:14:30 +0530

    Iterable and Iterator
    Every collection must implement Iterable interface.

--- src/CollectionTest.java
+++ src/CollectionTest.java
@@ -1,2 +1,24 @@
+import java.util.Iterator;
+import java.util.Objects;
+
 public class CollectionTest {
+    public static void main(String[] args) {
+        OurGenericList<Integer> list = new OurGenericList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> iterator = list.iterator();
+
+        while (iterator.hasNext())
+
+            System.out.println(iterator.next());
+
+        // OR  //
+
+        for (Object x : list)
+            System.out.println(x);
+
+    }
 }
--- src/OurGenericList.java
+++ src/OurGenericList.java
@@ -1,20 +1,47 @@
 import java.util.Iterator;
 
-public class OurGenericClass<T> implements Iterable{
-    private T[] items;
+public class OurGenericList<T> implements Iterable {
+    private final T[] items;
     private int size;
 
-    public OurGenericClass() {
+    public OurGenericList() {
         this.size = 0;
         this.items = (T[]) new Object[100];
     }
 
+    public void add(T item) {
+        items[size++] = item;
+    }
+
+    public T getItemAtIndex(int index) {
+        return items[index];
+    }
+
     @Override
     public Iterator iterator() {
-        return null;
+        return new OurGenericListIterator(this);
     }
 
 
-    private class Our
+    private class OurGenericListIterator<T> implements Iterator {
+
+        private final OurGenericList<T> list;
+        private int index = 0;
+
+        private OurGenericListIterator(OurGenericList<T> list) {
+            this.list = list;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return index < list.size;
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+    }
 
 }
